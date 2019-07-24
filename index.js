fd442f8d6c3238dcfc56fb68a4a32d6b1549c1a8
github username <github email address>
2019-07-24 16:54:30 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+   return string.replace(/[^A-Za-z0-9- ']+/g, '');
+    
+  }
+
+  static titleize(string) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let wordsArray = string.split(" ")
+    for (let n = 0; n < wordsArray.length; n++) {
+      if (n == 0) {
+        result.push(this.capitalize(wordsArray[n]))
+      } else {
+        if (exceptions.includes(wordsArray[n])) {
+          result.push(wordsArray[n])
+        } else {
+          result.push(this.capitalize(wordsArray[n]))
+        }
+      }
+    }
+return result.join(" ");
+  }
 }
+
