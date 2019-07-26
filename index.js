6991db99e1134ca88b5999ab893574b41cb62cf1
Jowita <jowita.lapies@gmail.com>
2019-07-26 13:58:14 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(s) {
+    return s.charAt( 0 ).toUpperCase() + s.slice( 1 );
+  }
+ 
+
+  static sanitize(s) { 
+    return s.replace(/[^A-Za-z0-9 '-]/g, "");
+
+  }
+
+  static titleize(s) {
+    const stopWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from"
+    ];
+    const splitSentence = s.split(" ");
+    const newArray = new Array();
+    for (let i = 0; i < splitSentence.length; i++) {
+      if (i === 0 || !stopWords.includes(splitSentence[i])) {
+        newArray.push(this.capitalize(splitSentence[i]));
+      } else {
+        newArray.push(splitSentence[i]);
+      }
+    }
+    return newArray.join(" ");
+  }
+  
 }
